8daa72a16894c58715198aad7a09338fcdfc8c5d
vovacelidze <louglescom@gmail.com>
2022-10-24 16:35:09 +0300

    first commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,15 @@
 const express = require('express')
-
 const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact
+} = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: await listContacts() })
 })
 
 router.get('/:contactId', async (req, res, next) => {
