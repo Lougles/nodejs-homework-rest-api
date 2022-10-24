e5b3ca0985bf8ae3520848c7eea695b7eeba3500
vovacelidze <louglescom@gmail.com>
2022-10-24 16:38:17 +0300

    added joi shema validation for contacts

---
+++ validation/schema.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const contactShema = Joi.object({
+  name: Joi.string()
+  .alphanum()
+  .min(3)
+  .max(10)
+  .required(),
+  email: Joi.string()
+  .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+  .required(),
+  phone: Joi.string()
+  .length(10).pattern(/^[0-9]+$/)
+  .required(),
+})
+
+module.exports = {
+  contactShema
+}
