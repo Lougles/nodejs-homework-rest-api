f87882ae45ed2873a8d6c9d6b067b29c015c70ff
vovacelidze <louglescom@gmail.com>
2022-10-25 13:37:58 +0300

    added connection to db, and created contact schema

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require('./app');
+require('dotenv').config()
+const mongoose = require('mongoose');
+// mongoose.connect(process.env.URL);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+
+app.listen(3000, async () => {
+  try {
+    await mongoose.connect(process.env.URL);
+    console.log("Server running. Use our API on port: 3000")
+  } catch (e) {
+    console.log(e.message);
+  }
 })
