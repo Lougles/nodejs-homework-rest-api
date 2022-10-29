4c9bae254ea4b20100b783d1683a690ad72732b1
vovacelidze <louglescom@gmail.com>
2022-10-29 19:45:37 +0300

    fix controller:

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -8,18 +8,18 @@ const {
 } = require('../services/contactsService')
 
 const listContactsController = async (req,res) => {
-    const result = await listContactsService();
-    res.json(result);
+  const result = await listContactsService();
+  res.json(result);
 }
 
 const getContactByIdController = async (req,res) => {  
-    const {status, message} = await getContactByIdService(req.params.id);
-    res.status(status).json(message);
+  const {status, message} = await getContactByIdService(req.params.id);
+  res.status(status).json(message);
 }
 
 const removeContactController = async (req,res) => {
-    const {status, message} = await removeContactService(req.params.id);
-    res.status(status).json(message);
+  const {status, message} = await removeContactService(req.params.id);
+  res.status(status).json(message);
 }
 
 const addContactController = async (req,res) => {
@@ -28,8 +28,8 @@ const addContactController = async (req,res) => {
 }
 
 const updateContactController = async (req,res) => {
-    const {status, message} = await updateContactService(req.params.id, req.body)
-    res.status(status).json(message);
+  const {status, message} = await updateContactService(req.params.id, req.body)
+  res.status(status).json(message);
 }
 
 const updateFavoriteController = async (req, res) => {
