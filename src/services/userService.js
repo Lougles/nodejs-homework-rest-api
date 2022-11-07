3c728fd1499a5b7063f193612371480844b1e799
vovacelidze <louglescom@gmail.com>
2022-11-07 11:11:56 +0200

    added login service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,7 @@
 const {User} = require('../models/userModel')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
 
 const registration = async(email, password) => {
@@ -11,7 +14,16 @@ const registration = async(email, password) => {
 
 const login = async(email, password) => {
   try {
-    
+    const user = await User.findOne({email});
+    if(!user) return {status: 404, message: `User is not found`}
+    if(!await bcrypt.compare(password, user.password)) return {status: 400, message: `Wrong password`} 
+    const token = jwt.sign({
+      _id: user._id,
+      createdAt: user.createdAt,
+    }, process.env.JWT);
+    user.token = token;
+    await user.save();
+    return {status: 200, message: {"token": token}}
   } catch (err) {
     return {status: 400, message: err.message}
   }
