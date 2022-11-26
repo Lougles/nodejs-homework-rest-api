47368e535402c343d78b7a687fcb760bb1f140f7
vovacelidze <louglescom@gmail.com>
2022-11-26 16:03:57 +0200

    added registration method to send an email

--- nodemailer.js
+++ nodemailer.js
@@ -1,27 +1,35 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config()
 
-const msg = {
-  from: 'louglescom@gmail.com',
-  to: 'chelidze.v.a@gmail.com',
-  subject: 'Nodemailer Testing',
-  text: 'Testing out first send'
+
+const sendMail = async(email, subject, text, hash) => {
+  const msg = {
+    from: 'louglescom@gmail.com',
+    to: email,
+    subject: subject,
+    text: `${text}/${hash}`
+  }
+  nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'louglescom@gmail.com',
+      pass: process.env.GOOGLE_PASSWORD
+    },
+    port: 465,
+    host: 'smtp.gmail.com'
+  })
+  .sendMail(msg, (err) => {
+    if(err){
+      return console.log('Error', err)
+    }
+    return console.log("Email sent")
+  })
 }
 
-nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'louglescom@gmail.com',
-    pass: process.env.GOOGLE_PASSWORD
-  },
-  port: 465,
-  host: 'smtp.gmail.com'
-})
-.sendMail(msg, (err) => {
-  if(err){
-    return console.log('Error', err)
-  }
-  return console.log("Email sent")
-})
 
- 
+sendMail('chelidze.v.a@icloud.com', 'Registration confirmation', 'To finish registration move to http:4040/api/contacts/user/verify', 'qwertyu')
+
+
+ module.exports = {
+  sendMail
+ }
